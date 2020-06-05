35eb33687fde0531471c93d5eff0629505b143b3
CD <mrrash91@gmail.com>
2020-06-05 15:46:41 +0800

    Add component folder & files

---
+++ components/Card.tsx
@@ -0,0 +1,5 @@
+const Card = (): JSX.Element => {
+  return <div className="border-solid border-2 bg-white rounded-md p-1 shadow-md h-auto">test</div>
+}
+
+export default Card
---
+++ components/Circle.tsx
@@ -0,0 +1,9 @@
+const Circle = (): JSX.Element => {
+  return (
+    <div className="border flex bg-red-300 border-solid rounded-full w-12 h-12 self-center justify-center items-center">
+      c
+    </div>
+  )
+}
+
+export default Circle
