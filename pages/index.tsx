aa6462a88f6774f38e311a227523a9008eb5031e
CD <mrrash91@gmail.com>
2020-06-05 15:47:09 +0800

    Apply component to main index file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,33 @@
+import Circle from 'components/Circle'
+import Card from 'components/Card'
+
+const Header = (): JSX.Element => {
+  return (
+    <div className="shadow-md">
+      <div className="bg-white flex h-12 px-3 py-0 font-bold text-lg items-center relative">
+        <div className="">title here</div>
+      </div>
+      <div className="bg-white h-48">hero here</div>
+    </div>
+  )
+}
+
+const Content = (): JSX.Element => {
+  return (
+    <div className="flex flex-1 justify-center">
+      <div className="flex border justify-center mt-5 flex-col w-screen max-w-screen-sm p-3">
+        <Circle />
+        <Card />
+      </div>
+    </div>
+  )
+}
+
 const Home = (): JSX.Element => {
   return (
-    <div className="flex">
-      <p>test</p>
+    <div>
+      <Header />
+      <Content />
     </div>
   )
 }
